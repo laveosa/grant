dbf183a63e395e23f1c5ef640229008d68a21f3d
laveosa <laveosa@yahoo.com>
2020-02-13 16:43:27 +0200

    Add - head layout and Bootstrap 4 lib

--- index.js
+++ index.js
@@ -2,24 +2,22 @@ const http = require("http");
 const ejs = require("ejs");
 const fs = require("fs");
 const dotenv = require("dotenv").config();
+const yargs = require("yargs");
 
 const server = http.createServer((req, res) => {
-  if (req.url == "/") {
+  if (req.url === "/") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
-    fs.readFile("./dev/views/home.ejs", "utf8", (err, data) => {
-      if (err) throw err;
 
-      let model = {
-        title: "Home page"
-      };
+    let model = {
+      title: "GRRR title"
+    };
 
-      res.end(ejs.render(data, model));
+    ejs.renderFile("./dev/views/home.ejs", model, (err, data) => {
+      res.end(data);
     });
   }
 });
-
-console.log(process.env.TEST);
 
 let POST = process.env.POST || 3000;
 server.listen(POST, err => err);
