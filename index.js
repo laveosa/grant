e08efcf02a2b02a04919e7c17707402fe3c37f4b
laveosa <laveosa@yahoo.com>
2020-02-13 19:20:54 +0200

    Add - tabs 'dashboard', 'messages', 'stats' in Home page and navigation tof them

--- index.js
+++ index.js
@@ -20,6 +20,67 @@ const server = http.createServer((req, res) => {
     let model = {
       nav: {
         selectedTab: "/"
+      },
+      content: {
+        contentId: null,
+        contentUrl: null
+      }
+    };
+
+    ejs.renderFile("./dev/views/home.ejs", model, (err, data) => {
+      res.end(data);
+    });
+  }
+
+  if (req.url === "/home/dashboard") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+
+    let model = {
+      nav: {
+        selectedTab: "/"
+      },
+      content: {
+        contentId: "dashboard",
+        contentUrl: "./home-dashboard.ejs"
+      }
+    };
+
+    ejs.renderFile("./dev/views/home.ejs", model, (err, data) => {
+      res.end(data);
+    });
+  }
+
+  if (req.url === "/home/messages") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+
+    let model = {
+      nav: {
+        selectedTab: "/"
+      },
+      content: {
+        contentId: "messages",
+        contentUrl: "./home-messages.ejs"
+      }
+    };
+
+    ejs.renderFile("./dev/views/home.ejs", model, (err, data) => {
+      res.end(data);
+    });
+  }
+
+  if (req.url === "/home/stats") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+
+    let model = {
+      nav: {
+        selectedTab: "/"
+      },
+      content: {
+        contentId: "stats",
+        contentUrl: "./home-stats.ejs"
       }
     };
 
@@ -46,8 +107,6 @@ const server = http.createServer((req, res) => {
   if (req.url === "/contacts") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
-
-    console.log("contacts server check point...");
 
     let model = {
       nav: {
