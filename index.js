9d432164dbcbe41732e1fbc6a492ecadd4f3e399
laveosa <laveosa@yahoo.com>
2020-02-13 17:33:00 +0200

    Add - new pages stub: 'Home', 'About', 'Contacts' and navigation for them

--- index.js
+++ index.js
@@ -6,14 +6,56 @@ const yargs = require("yargs");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
+    res.writeHead(302, {
+      Location: "/home"
+    });
+
+    res.end();
+  }
+
+  if (req.url === "/home") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
 
     let model = {
-      title: "GRRR title"
+      nav: {
+        selectedTab: "/"
+      }
     };
 
     ejs.renderFile("./dev/views/home.ejs", model, (err, data) => {
+      res.end(data);
+    });
+  }
+
+  if (req.url == "/about") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+
+    let model = {
+      nav: {
+        selectedTab: "/about"
+      }
+    };
+
+    ejs.renderFile("./dev/views/about.ejs", model, (err, data) => {
+      res.end(data);
+    });
+  }
+
+  if (req.url === "/contacts") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+
+    console.log("contacts server check point...");
+
+    let model = {
+      nav: {
+        selectedTab: "/contacts"
+      }
+    };
+
+    ejs.renderFile("./dev/views/contacts.ejs", model, (err, data) => {
       res.end(data);
     });
   }
