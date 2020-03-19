4590c76c5112ae2415d8031a24db3251cb2d75ba
laveosa <laveosa@yahoo.com>
2020-03-19 21:25:52 +0200

    navigate to home page

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-const dotenv = require("dotenv").config();
 const express = require("express");
-const hbs = require("express-handlebars");
 const path = require("path");
+const dotenv = require("dotenv").config();
+const exphbs = require("express-handlebars");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "/app/client")));
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: "hbs",
+    layoutsDir: path.join(__dirname, "app/server/views/layouts/"),
+    partialsDir: path.join(__dirname, "app/server/views/partials/")
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "app/server/views/"));
+
+app.use(express.static(path.join(__dirname, "app/client/")));
+
+app.get("/", (req, res) => {
+  res.render("home", {});
+});
 
 app.listen(PORT, () => {
   console.log("server runing...");
