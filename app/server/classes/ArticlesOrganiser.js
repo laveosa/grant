cdd5256f5c0a0959d568bede09fcfd58c8798f89
laveosa <laveosa@yahoo.com>
2020-03-26 01:16:48 +0200

    (server)Update - Articles Organiser class logic

--- app/server/classes/ArticlesOrganiser.js
+++ app/server/classes/ArticlesOrganiser.js
@@ -4,17 +4,36 @@ const DBpath = "./app/server/DB/files/page-articles.json";
 
 class ArticlesOrganiser {
   constructor() {
-    this.allArticles = {};
+    this.articles = [];
   }
   organise(url) {
     return {
-      mainTitle: "Page Title"
+      mainTitle: this.getPageMainTitle(url)
     };
   }
   getArticlesFromDB() {
     return new Promise((resolve, reject) => {
-      resolve(true);
+      fs.readFile(DBpath, "utf8", (err, data) => {
+        if (!data || data.length <= 0) return false;
+
+        data = JSON.parse(data);
+        this.articles = data;
+        resolve(this.articles);
+      });
     });
+  }
+  getPageMainTitle(url) {
+    if (!url || url.length <= 0) return false;
+
+    let mainTitle = "no title for this page";
+
+    this.articles.forEach((elem, idx) => {
+      if (elem.url == url) {
+        mainTitle = elem.mainTitle;
+      }
+    });
+
+    return mainTitle;
   }
   initialize() {
     return Promise.all([this.getArticlesFromDB()]).then(response => {
