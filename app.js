036feee15f5cf71678e68172e07b0ba3b0423ae7
laveosa <laveosa@yahoo.com>
2020-03-25 21:33:37 +0200

    (client) Add - navigation layouts and logic

--- app.js
+++ app.js
@@ -22,28 +22,99 @@ app.set("views", path.join(__dirname, "app/server/views/"));
 app.use(express.static(path.join(__dirname, "app/client/")));
 app.use(express.json());
 
+let model = {
+  nav: {
+    tabs: [
+      {
+        url: "/home",
+        name: "home",
+        active: false
+      },
+      {
+        url: "/users",
+        name: "users",
+        active: false
+      },
+      {
+        url: "/companies",
+        name: "companies",
+        active: false
+      },
+      {
+        url: "/vehicles",
+        name: "vehicles",
+        active: false
+      },
+      {
+        url: "/foods",
+        name: "foods",
+        active: false
+      },
+      {
+        url: "/log",
+        name: "log",
+        active: false
+      }
+    ],
+    activateTab: url => {
+      model.nav.tabs.forEach((tab, idx) => {
+        tab.active = false;
+
+        if (tab.url === url) {
+          tab.active = true;
+        }
+      });
+    }
+  },
+  text: {
+    mainTitle: "Main page"
+  }
+};
+
 app.get("/", (req, res) => {
   res.redirect("/home");
 });
 
 app.get("/home", (req, res) => {
-  res.render("pages/home", {});
+  model.nav.activateTab(req.url);
+  model.text.mainTitle = "Home page";
+
+  res.render("pages/home", model);
 });
 
 app.get("/users", (req, res) => {
-  res.render("pages/users");
+  model.nav.activateTab(req.url);
+  model.text.mainTitle = "Users page";
+
+  res.render("pages/users", model);
 });
 
 app.get("/foods", (req, res) => {
-  res.render("pages/foods");
+  model.nav.activateTab(req.url);
+  model.text.mainTitle = "Foods page";
+
+  res.render("pages/foods", model);
 });
 
 app.get("/companies", (req, res) => {
-  res.render("companies");
+  model.nav.activateTab(req.url);
+  model.text.mainTitle = "Companies page";
+
+  res.render("pages/companies", model);
 });
 
 app.get("/vehicles", (req, res) => {
-  res.render("pages/vehicles");
+  model.nav.activateTab(req.url);
+  model.text.mainTitle = "Vehicles page";
+
+  res.render("pages/vehicles", model);
+});
+
+app.get("/log", (req, res) => {
+  model.nav.activateTab(req.url);
+  model.text.mainTitle = "Log page";
+
+  res.render("pages/log", model);
 });
 
 app.use("/api/users", UsersRouter);
